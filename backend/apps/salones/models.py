8a48a8994d4b48c125eac2c13d187ddc8e38ebb0
AndreCamx2 <andresfloo27@gmail.com>
2026-09-20 11:12:44 -0500

    chore: dockerizar backend y PostgreSQL para desarrollo local

--- backend/apps/salones/models.py
+++ backend/apps/salones/models.py
@@ -16,6 +16,7 @@ class Salon(models.Model):
     nombre = models.CharField(max_length=100)
     altura = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     ancho = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    foto_url = models.URLField(blank=True)
 
     def __str__(self):
         return f"{self.nombre} ({self.sitio.nombre})"
